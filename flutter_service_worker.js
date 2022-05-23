fefee66e9e267d6f65af76eaa42e34bcaa35d348
Marrawi1994 <35656567+Marrawi1994@users.noreply.github.com>
2022-05-23 12:29:41 +0300

    updarte

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b17560ff120e78c0f353c3b51795401f",
-"/": "b17560ff120e78c0f353c3b51795401f",
+"index.html": "20496aa73ab08a2d38ab449a36d5f16f",
+"/": "20496aa73ab08a2d38ab449a36d5f16f",
 "main.dart.js": "72231454c6a1652d0e94bce5ade9e33a",
 "manifest.json": "ee159ae81f8f022d114dc8d0e06bab9f",
 "version.json": "f0aa2613ca08d4fad50e06af77b940f7"
